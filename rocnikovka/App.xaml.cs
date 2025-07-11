f75130ce806bb85ae0abd6fd206a70c1c4953258
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-11 19:17:23 +0200

    Rocnikovka

---
+++ rocnikovka/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Rocnikovka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
