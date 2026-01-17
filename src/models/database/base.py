be8fd9ec030e76ea5c32c3207351b8257a49d47c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 16:06:36 +0100

    Cleanup

--- src/models/database/base.py
+++
@@ -1,7 +0,0 @@
-"""Base model for SQLAlchemy ORM classes."""
-
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):  # pylint: disable=too-few-public-methods
-    """Base class for all SQLAlchemy ORM models."""
